da058b09ab306c8c2fdb3c7cebfe230cb338e094
Azalurg <91213389+Azalurg@users.noreply.github.com>
2025-02-18 12:56:12 +0100

    Update champions endpoint

--- app/champions/router.py
+++ app/champions/router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
+from sqlalchemy.orm import selectinload
 from sqlmodel import Session, select
+import json
 
 from app.database import engine
-from app.champions.models import Champion
-
+from app.champions.models import Champion, Race
 
 router = APIRouter(prefix="/champions", tags=["champions"])
 
@@ -11,5 +12,24 @@ router = APIRouter(prefix="/champions", tags=["champions"])
 @router.get("/")
 def get_champions():
     with Session(engine) as session:  # TODO: Move to service
-        champions = session.exec(select(Champion).limit(10)).all()
-    return champions
+        champions = session.exec(
+            select(Champion)
+            .order_by(Champion.level.desc())
+            .options(
+                selectinload(Champion.race),
+                selectinload(Champion.character_class),
+                selectinload(Champion.profession),
+            )
+            .limit(10)
+        ).all()
+
+    return [
+        {
+            "name": champ.name,
+            "level": champ.level,
+            "race": champ.race.name,
+            "class": champ.character_class.name,
+            "profession": champ.profession.name,
+        }
+        for champ in champions
+    ]
